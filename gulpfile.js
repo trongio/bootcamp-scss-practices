ee87f433a2337df537adb63aaddbff49f6a69cfe
trongio <giokakabadze50@gmail.com>
2019-05-14 21:12:57 +0400

    feat(2-Preprocessing-Core): Finish practice-1

--- gulpfile.js
+++ gulpfile.js
@@ -18,15 +18,12 @@ gulp.task('clean:html', () => {
     ]);
 });
 
-<<<<<<< HEAD
-=======
 gulp.task('clean:images', () => {
     return del([
         'dist/images/*',
     ]);
 });
 
->>>>>>> 0de23440822fa6572896dbeba008861527a08357
 gulp.task('sass', function () {
   return gulp.src('source/styles/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -38,26 +35,15 @@ gulp.task('html', function(){
       .pipe(gulp.dest('dist/'));
 });
 
-<<<<<<< HEAD
-=======
 gulp.task('images', function() {
     return gulp.src('source/images/**/*.*')
         .pipe(gulp.dest('dist/images/'));
 });
 
->>>>>>> 0de23440822fa6572896dbeba008861527a08357
 gulp.task('watch', function() {
     gulp.watch('source/*.html', (done) => {
         gulp.series(['clean:html', 'html'])(done);
     });
-<<<<<<< HEAD
-    gulp.watch('source/styles/*.scss', (done) => {
-        gulp.series(['clean:css', 'sass'])(done);
-    });
-});
-
-const init = ['clean:css', 'sass', 'clean:html', 'html']
-=======
     gulp.watch('source/styles/**/*.scss', (done) => {
         gulp.series(['clean:css', 'sass'])(done);
     });
@@ -74,6 +60,5 @@ const init = [
     'clean:images',
     'images'
 ];
->>>>>>> 0de23440822fa6572896dbeba008861527a08357
 
 gulp.task('default', gulp.series([...init,'watch']));
